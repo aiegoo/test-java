d119defbfd6c935b83c5b2eb3e19d41ee286c588
tonyleekorea <42961200+aiegoo@users.noreply.github.com>
2024-09-05 05:26:09 +0900

    maintest

---
+++ public class MainTest {.java
@@ -0,0 +1,11 @@
+public class MainTest {
+  public static void main(String[] args) {
+    testMain();
+  }
+
+  public static void testMain() {
+    // Test case 1: str = "agile", x = {1, 2, 3, 4, 5}
+    // Expected output: e 1 l 2 i 3 g 4 a 5
+    Main.main(new String[]{});
+  }
+}
